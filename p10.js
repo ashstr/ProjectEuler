d144eecc2b4d1bfe7415a4c00c0b61707f5565d7
Ashraf Abd Elsatar <ashraf.elkoly@gmail.com>
2016-08-21 16:52:53 +0200

    solution of p10

---
+++ p10.js
@@ -0,0 +1,30 @@
+
+(function p10() {
+  // 2e+6 maxValue
+  var i = 2e+6;
+  var sum = 0;
+
+  
+  while (i >= 2) {
+    if (isPrime(i) === true) {
+      sum += i;
+    }
+
+    i--;
+  }
+
+  function isPrime(n) {
+    var j = Math.floor(Math.sqrt(n));
+
+    while (j >= 2) {
+      if (n % j === 0) {
+        return false;
+      }
+      j--;
+    }
+
+    return true;
+  }
+
+  return sum;
+}());
